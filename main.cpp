5717a7bd7cd3afcf0151577ccb70cf7476c82efe
LamontSims11 <117595824+LamontSims11@users.noreply.github.com>
2022-11-11 22:56:33 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,11 @@
 using namespace std;
 int main()
 {
-    int a, b, x, d;
+    int a, b;
     bool is_prime = true;
 
     cin >> b;
     
-    x == b%2;
-    d == 1;
 
     if (b == 0 || b == 1)
       {
@@ -29,8 +27,28 @@ int main()
 
    if (is_prime)
       cout << "Number " << b << " is prime!" << endl;
-   if (b%2 == d++)
-      cout << x << endl;
+   if (b%2 == 0)
+     cout << 2 << endl;
+   if (b%3 == 0)
+     cout << 3 << endl;
+   if (b%5 == 0)
+     cout << 5 << endl;
+   if (b%7 == 0)
+     cout << 7 << endl;
+   if (b%11 == 0)
+     cout << 11 << endl;
+   if (b%13 == 0)
+     cout << 13 << endl;
+   if (b%17 == 0)
+     cout << 17 << endl;
+   if (b%19 == 0)
+     cout << 19 << endl;
+   if (b%23 == 0)
+     cout << 23 << endl;
+   if (b%29 == 0)
+     cout << 29 << endl;
+   if (b%31 == 0)
+      cout << 31 << endl;
       return 0;
    
 }
