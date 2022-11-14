e2e33e193ffc4b6a246914b7c4241be504e8a90b
LamontSims11 <117595824+LamontSims11@users.noreply.github.com>
2022-11-14 17:32:47 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 // Lamont Sims         J00968337
 // Program #22         Page #264
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 int main()
 {
@@ -31,27 +30,27 @@ int main()
    }
 
    if (b%2 == 0)
-     cout << 2 << endl;
+     cout << 2 << " ";
    if (b%3 == 0)
-     cout << 3 << endl;
+     cout << 3 << " ";
    if (b%5 == 0)
-     cout << 5 << endl;
+     cout << 5 << " ";
    if (b%7 == 0)
-     cout << 7 << endl;
+     cout << 7 << " ";
    if (b%11 == 0)
-     cout << 11 << endl;
+     cout << 11 << " ";
    if (b%13 == 0)
-     cout << 13 << endl;
+     cout << 13 << " ";
    if (b%17 == 0)
-     cout << 17 << endl;
+     cout << 17 << " ";
    if (b%19 == 0)
-     cout << 19 << endl;
+     cout << 19 << " ";
    if (b%23 == 0)
-     cout << 23 << endl;
+     cout << 23 << " ";
    if (b%29 == 0)
-     cout << 29 << endl;
+     cout << 29 << " ";
    if (b%31 == 0)
-      cout << 31 << endl;
+      cout << 31 << " ";
 
    return 0;
 
